8d3b46f21cf3fbf278e1216e69c8a2ddb31c58c9
IceCrystal21 <master0sword@gmail.com>
2026-09-08 15:43:26 -0400

    Added skeleton cdoe to test api

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
+
+
+app.get("/app/users", (req, res) => {
+    const users = [
+        { id: 1, name: 'Alice' },
+        { id: 2, name: 'Bob' }
+    ];
+    res.status(200).json(users);
+})
 
 app.listen(PORT , () => {
-    console.log('Running on port ${PORT}')
+    console.log(`Running on port: ${PORT}`)
 });
